d6af0d5fa85d582a42726b1ac08725845c78cb92
JohanOrrego <johan420@hotmail.com>
2018-04-23 15:08:48 -0500

    inicio proyecto

---
+++ mundial/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.conf.urls import include, url
+from django.contrib.auth.decorators import login_required
+import views
+from .views import *
+
+urlpatterns = [
+
+]
